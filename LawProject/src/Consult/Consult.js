c08496b9f7b635d2a54c1d45995355cd0d2a6d39
zhouhong <q15230189155@163.com>
2021-02-23 18:27:00 +0800

    2/23

--- LawProject/src/Consult/Consult.js
+++ LawProject/src/Consult/Consult.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
-import { View,Text } from 'react-native'
+import { View,Text,Dimensions } from 'react-native'
+
+import pxToDp from '../pxToDp';
+
+const {width}=Dimensions.get('window')
 
 export default class Talk extends Component {
     render() {
         return (
             <View>
-                <Text>111</Text>
+                <View style={{width:width,height:pxToDp(50),backgroundColor:'yellow'}}>
+
+                </View>
             </View>
         )
     }
--- LawProject/src/NavBar/NavBar.js
+++ LawProject/src/NavBar/NavBar.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react'
-import { StatusBar, Text, View } from 'react-native'
+import { StatusBar, Text, View ,TouchableOpacity} from 'react-native'
 import pxToDp from '../pxToDp';
 
 export default class NavBar extends Component {
     render() {
-        return (
-            <View>
-               <View style={{height:pxToDp(75),backgroundColor:'#CE0000',flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
-                    <Text style={{fontSize:pxToDp(30),color:'white'}}>{this.props.name}</Text>
-               </View>
-            </View>
-        )
+        if (this.props.name == '咨询') {
+            return (
+                <View>
+                    <View style={{ height: pxToDp(75), backgroundColor: '#CE0000', flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center' }}>
+                        <Text style={{ fontSize: pxToDp(30), color: 'white' ,marginRight:pxToDp(170)}}>{this.props.name}</Text>
+                        <TouchableOpacity>
+                            <View style={{width:pxToDp(100),height:pxToDp(38),flexDirection:'row',marginRight:pxToDp(20),justifyContent:'center',alignItems:'center',backgroundColor:'white',borderRadius:pxToDp(50)}}>
+                                <Text style={{color:'#CE0000',fontSize:pxToDp(25)}}>发布</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            )
+        }
+        else {
+            return (
+                <View>
+                    <View style={{ height: pxToDp(75), backgroundColor: '#CE0000', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                        <Text style={{ fontSize: pxToDp(30), color: 'white' }}>{this.props.name}</Text>
+                        
+                    </View>
+                </View>
+            )
+        }
+
     }
 }
